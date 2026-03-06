e9826e0e2e3d477fcd51f0604f24c79922e77e1c
Guilherme Bastos <guilherme.mb.dev@gmail.com>
2026-03-06 20:04:05 -0300

    feat(ap03-src-asset): adiciona pagina principal atraves de js

--- ap03-react-componentes-jsx/src/App.jsx
+++ ap03-react-componentes-jsx/src/App.jsx
@@ -1,6 +1,35 @@
 const App = () => {
-  return <div>
-    Meu primeiro componente react
+  const textoDoLable = 'Name:'
+  const textoDoBotao = 'Enviar'
+  return <div style={{
+    margin: 'auto',
+    width: 576,
+    backgroundColor: '#EEE',
+    padding: 12,
+    borderRadius: 8
+  }}>
+    <label htmlFor='nome' style={{
+      display: 'block',
+      marginBottom: 4
+    }}>{textoDoLable}</label>
+    <input type='text' id='nome' style={{
+      paddingTop: 8,
+      paddingBottom: 8,
+      borderStyle: 'hidden',
+      width: '100%',
+      borderRadius: 8,
+      outline: 'none'
+    }}/>
+    <button style={{
+      marginTop: 12,
+      paddingTop: 8,
+      paddingBottom: 8,
+      backgroundColor: 'blueviolet',
+      color: 'white',
+      border: 'none',
+      width: '100%',
+      borderRadius: 8
+    }}>{textoDoBotao}</button>
   </div>
 }
 
